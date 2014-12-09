4526f8e4af2a60afd39d0e4f0f183534ccbb122f
Cies Breijs <cies@kde.nl>
2014-12-09 12:46:45 +0100

    Update wp_cache.rb

--- app/models/concerns/wp_cache.rb
+++ app/models/concerns/wp_cache.rb
@@ -4,6 +4,19 @@ module WpCache
   extend ActiveSupport::Concern
 
   module ClassMethods
+    def sync_cache(wp_type, wp_id)
+      WpGetWorker.perform_async(self, wp_type, wp_id)
+    end
+  
+    def purge_cache(wp_id)
+      m = self.find(wp_id)
+      if m
+        m.destroy
+      else
+        logger.warn "Could not find #{self} with id #{wp_id}."
+      end
+    end
+    
     def retrieve_and_update_wp_cache(wp_type, wp_id)
       response = Faraday.get "#{Settings.wordpress_url}/#{wp_type}/#{wp_id}"
       wp_json = JSON.parse(response.body)
