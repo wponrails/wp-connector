e6b290cd56cf8a6b4eb4cc75926f6f3dff4a7d77
Sebastiaan de Geus <sebastiaan@hoppinger.com>
2015-01-26 14:14:04 +0100

    disable retries, it most likely fails because of code mismatch

--- app/workers/wp_get_worker.rb
+++ app/workers/wp_get_worker.rb
@@ -1,5 +1,6 @@
 class WpGetWorker
   include Sidekiq::Worker
+  sidekiq_options :retry => false
 
   def perform(model_name, wp_type, wp_id)
     model_name.constantize.retrieve_and_update_wp_cache(wp_type, wp_id)
