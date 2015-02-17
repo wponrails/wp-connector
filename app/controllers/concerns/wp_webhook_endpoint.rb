af51b58db28274fb1b4e099d46cc914c24ba3c25
Dunya Kirkali <dunyakirkali@gmail.com>
2015-02-17 19:51:53 +0100

    clenaan up

--- app/controllers/concerns/wp_webhook_endpoint.rb
+++ app/controllers/concerns/wp_webhook_endpoint.rb
@@ -1,14 +1,6 @@
 module WpWebhookEndpoint
   extend ActiveSupport::Concern
 
-  # included do
-  #   after_filter :render_nothing
-  # end
-
-  # def render_nothing
-  #   render nothing: true
-  # end
-
   def wp_id_from_params
     # The `parent_ID` has precedence over `ID` as the former contains
     # the actual ID used by WP in case multiple versions exist.
--- app/models/concerns/wp_cache.rb
+++ app/models/concerns/wp_cache.rb
@@ -1,5 +1,6 @@
 require 'faraday'
 
+# WpCache
 module WpCache
   extend ActiveSupport::Concern
 
@@ -21,25 +22,22 @@ module WpCache
       begin
         self.joins(:post).where('posts.post_id = ?', wp_id).first!.destroy
       rescue
-        logger.warn "Could not find #{self} with id #{wp_id}."
+        logger.warn "Could not find #{ self } with id #{ wp_id }."
       end
     end
 
     def create_or_update(wp_type, wp_id)
       return unless wp_id.is_a? Fixnum or wp_id.is_a? String
-
-      response = Faraday.get "#{Settings.wordpress_url}?json_route=/#{wp_type}/#{wp_id}"
+      response = Faraday.get "#{ Settings.wordpress_url }?json_route=/#{ wp_type }/#{ wp_id }"
       wp_json = JSON.parse(response.body)
-
       #WP API will return a 'json_no_route' code if the route is incorrect or the specified entry is none existant
       #If so, do not 'first_or_create'
       return if wp_json["code"] == "json_no_route"
-
-      self.joins(:post).where(posts: {post_id: wp_id}).first_or_create.update_wp_cache(wp_json)
+      joins(:post).where(posts: { post_id: wp_id }).first_or_create.update_wp_cache(wp_json)
     end
 
     def create_or_update_all(wpclass)
-      response = Faraday.get "#{Settings.wordpress_url}?json_route=/#{wpclass.pluralize.downcase}"
+      response = Faraday.get "#{ Settings.wordpress_url }?json_route=/#{ wpclass.pluralize.downcase }"
       wp_json = JSON.parse(response.body)
       ids = []
       wp_json.each do |json|
