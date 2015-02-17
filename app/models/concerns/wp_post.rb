5a68d51b75911d0cea808ad82b3e8dc33985643b
jeroenrietveld <jeroen_rietveld13@msn.com>
2015-02-17 20:35:53 +0100

    Fix incorrect class method

--- app/models/concerns/wp_post.rb
+++ app/models/concerns/wp_post.rb
@@ -5,21 +5,21 @@ module WpPost
     serialize :acf_fields
   end
 
+  def update_post(json)
+    mappable_wordpress_attributes.each do |wp_attribute|
+      send(wp_attribute, json[wp_attribute])
+    end
+
+    self.wp_id        = json['ID']
+    self.author_id    = json['author']
+    self.published_at = json['date']
+    self.order        = json['menu_order']
+    save!
+  end
+
   class << self
     def mappable_wordpress_attributes
       %w( slug title status content excerpt acf_fields )
-    end
-
-    def create_post(json)
-      mappable_wordpress_attributes.each do |wp_attribute|
-        send(wp_attribute, json[wp_attribute])
-      end
-
-      self.wp_id        = json['ID']
-      self.author_id    = json['author']
-      self.published_at = json['date']
-      self.order        = json['menu_order']
-      save!
     end
   end
 end
