c9c1fb1d093fac1f82145829fec19b32f6f3521c
Dunya Kirkali <dunyakirkali@gmail.com>
2015-02-18 00:30:08 +0100

    configure rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,13 @@
 require 'bundler/setup'
 Bundler.setup
 
-require 'wp-connector'
+# require 'wp-connector'
+require 'rspec/its'
+
+Dir["./spec/support/**/*.rb"].sort.each { |f| require f}
 
 RSpec.configure do |config|
-
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+  config.order = 'random'
 end
