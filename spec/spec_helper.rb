1523f49ed0fe121d7f26f4557dd4bda0f15bfcc5
Dunya Kirkali <dunyakirkali@gmail.com>
2015-02-18 01:03:38 +0100

    require needed gems for test env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,13 @@
 require 'bundler/setup'
 Bundler.setup
 
-# require 'wp-connector'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require 'rspec/its'
+require 'sidekiq'
+require 'sidekiq/testing'
+Sidekiq::Testing.fake!
+require 'wp-connector'
 
 Dir["./spec/support/**/*.rb"].sort.each { |f| require f }
 
@@ -10,4 +15,9 @@ RSpec.configure do |config|
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
   config.order = 'random'
+
+  config.before(:each) do | example |
+    # Clears out the jobs for tests using the fake testing
+    Sidekiq::Worker.clear_all
+  end
 end
