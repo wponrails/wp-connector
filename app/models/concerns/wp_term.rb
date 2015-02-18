8946c091099f7af2ff0b3c1c36564276e1f9cbae
Dunya Kirkali <dunyakirkali@gmail.com>
2015-02-18 15:27:31 +0100

    add concern for wp_term

---
+++ app/models/concerns/wp_term.rb
@@ -0,0 +1,24 @@
+module WpTerm
+  extend ActiveSupport::Concern
+
+  def update_post(json)
+    self.class.mappable_wordpress_attributes.each do |wp_attribute|
+      send("#{wp_attribute}=", json[wp_attribute])
+    end
+
+    self.wp_id        = json['ID']
+    save!
+  end
+
+  module ClassMethods
+
+    def mappable_wordpress_attributes
+      %w( name slug description count )
+    end
+
+    def wp_type
+      self.to_s.underscore.pluralize
+    end
+
+  end
+end
