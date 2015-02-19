3fba70feda022bcc3fd162e2bfb7624b04cd7d0a
janmetten <janmetten@hoppinger.com>
2015-02-19 16:00:12 +0100

    Fixed parent in term

--- app/models/concerns/wp_term.rb
+++ app/models/concerns/wp_term.rb
@@ -8,8 +8,8 @@ module WpTerm
 
     self.wp_id        = json['ID']
     if json['parent']
-      update_term(json['parent'])
-      self.parent_id = self.class.where('wp_id = ?', json['parent']['ID']).first.id
+      self.class.where(wp_id: json['parent']['ID']).first_or_create.update_wp_cache(json['parent'])
+      self.parent_id = self.class.where(wp_id: json['parent']['ID']).first.id
     end
 
     save!
