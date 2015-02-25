a34f58b601111c34244285413b1e05416100fd01
Dunya Kirkali <dunyakirkali@gmail.com>
2015-02-25 17:41:05 +0100

    create_or_update_all now uses wp_type instead of pluralising the class name

--- app/models/concerns/wp_cache.rb
+++ app/models/concerns/wp_cache.rb
@@ -49,7 +49,7 @@ module WpCache
     # Removes records with unknown IDs.
     #
     def create_or_update_all
-      wp_json = get_from_wp_api self.to_s.underscore.pluralize
+      wp_json = get_from_wp_api wp_type
       ids = wp_json.map do |json|
         wp_id = json['ID']
         where(wp_id: wp_id).first_or_initialize.update_wp_cache(json)
