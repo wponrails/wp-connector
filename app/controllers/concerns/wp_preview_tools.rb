8d9a46eb81910e4d5986f3a39815cf1baf0bd399
Sebastiaan de Geus <sebastiaan@hoppinger.com>
2015-02-27 12:28:15 +0100

    Add comments, removed ClassMethods wrapper, Add retries for previews

--- app/controllers/concerns/wp_preview_tools.rb
+++ app/controllers/concerns/wp_preview_tools.rb
@@ -5,15 +5,33 @@
 module WpPreviewTools
   extend ActiveSupport::Concern
 
-  module ClassMethods
-    def validate_preview_token(wp_post_model)
-      return wp_post_model.status == "publish"
-      head :forbidden unless params[:token] == token(wp_post_model)  # TODO (cies): check if we have params[] here!
-    end
+  #
+  # Validates preview tokens for wp_post_models that are not published.
+  # for instance the statuses `draft` and `pending`.
+  #
+  def validate_preview_token(wp_post_model)
+    return wp_post_model.status == "publish"
+    head :forbidden unless params[:token] == token(wp_post_model)
+  end
 
-    def token(wp_post_model)
-      hash_inputs = Rails.configuration.x.wp_connector_secret + wp_post_model.slug
-      Digest::SHA2.new(256).hexdigest hash_inputs
-    end
+  #
+  # Creates a token to verify previews requests
+  #
+  def token(wp_post_model)
+    hash_inputs = Rails.configuration.x.wp_connector_secret + wp_post_model.slug
+    Digest::SHA2.new(256).hexdigest hash_inputs
+  end
+
+  #
+  # Retries loading of a WpCache model, if it was not found the first time.
+  # This to avoid NotFound errors due to the delaying of WP API calls.
+  #
+  def retry_when_preview(retrying=false, &block)
+    raise "retry_when_preview requires a block" unless block_given?
+    return block.call
+  rescue ActiveRecord::ActiveRecordError => e
+    raise e if !params[:preview] || retrying
+    sleep 1.5
+    return retry_when_preview(true, &block)
   end
 end
