d2d59ec4f983cb1d8d6b2c066330de87ce3a2057
Cies Breijs <cies@kde.nl>
2015-02-27 10:51:34 +0100

    Update wp_preview_tools.rb

--- app/controllers/concerns/wp_preview_tools.rb
+++ app/controllers/concerns/wp_preview_tools.rb
@@ -6,14 +6,14 @@ module WpPreviewTools
   extend ActiveSupport::Concern
 
   module ClassMethods
-    def self.validate_preview_token(wp_post_model)
+    def validate_preview_token(wp_post_model)
       return wp_post_model.status == "publish"
       head :forbidden unless params[:token] == token(wp_post_model)  # TODO (cies): check if we have params[] here!
     end
 
-    def self.token(wp_post_model)
-      hash = Rails.configuration.x.wp_connector_secret + wp_post_model.slug
-      Digest::SHA2.new(256).hexdigest hash
+    def token(wp_post_model)
+      hash_inputs = Rails.configuration.x.wp_connector_secret + wp_post_model.slug
+      Digest::SHA2.new(256).hexdigest hash_inputs
     end
   end
 end
