6266d58d4035c1875a07e5153547723443891124
Sebastiaan de Geus <sebastiaan@hoppinger.com>
2015-02-27 12:27:09 +0100

    Remove this default scope for now. It makes previews complicated.

--- app/models/concerns/wp_post.rb
+++ app/models/concerns/wp_post.rb
@@ -17,14 +17,6 @@ module WpPost
   end
 
   module ClassMethods
-    #
-    # By default only query on published WpPosts.
-    # Exclude 'draft' or 'pending' statusses.
-    #
-    def default_scope
-      where status: "publish"
-    end
-
     def mappable_wordpress_attributes
       %w( slug title status content excerpt acf_fields )
     end
