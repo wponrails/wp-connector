b37d5eff3a7cf5f698fabd62dc5ab42e664a0785
Sebastiaan de Geus <sebastiaan@hoppinger.com>
2015-07-07 15:18:27 +0200

    add wp_redirect concern

---
+++ app/models/concerns/wp_redirect.rb
@@ -0,0 +1,24 @@
+module WpRedirect
+  extend ActiveSupport::Concern
+
+  def update_wp_redirect_attributes(json)
+    return unless json.is_a?(Hash)
+
+    self.wp_id = json['id']
+    self.from  = json['from']
+    self.to    = json['to']
+
+    save!
+  end
+
+  # implements class methods
+  module ClassMethods
+    def mappable_wordpress_attributes
+      %w( name slug description count )
+    end
+
+    def wp_type
+      to_s.demodulize.underscore.pluralize
+    end
+  end
+end
