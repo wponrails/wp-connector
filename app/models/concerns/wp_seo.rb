8b30da57956eaf53c476906e2a999cc65cf3b2b6
jeroenrietveld <jeroen_rietveld13@msn.com>
2015-08-27 14:53:39 +0200

    Allow calling update_wp_seo_attributes without save

--- app/models/concerns/wp_seo.rb
+++ app/models/concerns/wp_seo.rb
@@ -8,6 +8,10 @@ module WpSEO
   def update_wp_seo_attributes(json)
     return unless json.is_a?(Hash)
     self.seo_fields = json['seo_fields']
+  end
+
+  def update_wp_seo_attributes!(json)
+    update_wp_seo_attributes(json)
     save!
   end
 end
