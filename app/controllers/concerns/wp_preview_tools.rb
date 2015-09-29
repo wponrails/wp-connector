cbee79922b43dac3807d1ae214b20f20f12bdf00
jeroenrietveld <jeroen_rietveld13@msn.com>
2015-09-29 13:41:43 +0200

    Fix preview functionality

--- app/controllers/concerns/wp_preview_tools.rb
+++ app/controllers/concerns/wp_preview_tools.rb
@@ -5,6 +5,8 @@
 module WpPreviewTools
   extend ActiveSupport::Concern
 
+  THRESHOLD = 5
+
   #
   # Validates preview tokens for wp_post_models that are not published.
   # for instance the statuses `draft` and `pending`.
@@ -34,12 +36,15 @@ module WpPreviewTools
   # Retries loading of a WpCache model, if it was not found the first time.
   # This to avoid NotFound errors due to the delaying of WP API calls.
   #
-  def retry_when_preview(retrying = false, &block)
+  def retry_when_preview(retrying = false, threshold = 0, &block)
     raise "retry_when_preview requires a block" unless block_given?
     return block.call
   rescue ActiveRecord::ActiveRecordError => e
-    raise e if !params[:preview] || retrying
+    raise e unless params[:preview] || retrying
+    return nil if THRESHOLD < threshold
+
     sleep 1.5
-    return retry_when_preview(true, &block)
+
+    return retry_when_preview(true, threshold + 1, &block)
   end
 end
